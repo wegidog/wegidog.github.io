e93bfd50f26342faac9d20a6c2e99e063ed08480
wegidog <69294454+wegidog@users.noreply.github.com>
2023-04-19 12:00:43 +0800

    fix: i18n.js 修復3

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -1,34 +1,41 @@
 class I18nManager {
   constructor() {
     this.currentLang = "en";
-    this.translations = {};
-    this.loadTranslations();
+    this.translations = {
+      en: {},
+      zh: {}
+    };
   }
 
-  loadTranslations() {
-    const enTranslationsPromise = fetch("en-US.json").then(response => response.json());
-    const zhTranslationsPromise = fetch("zh-TW.json").then(response => response.json());
-    Promise.all([enTranslationsPromise, zhTranslationsPromise]).then(([enTranslations, zhTranslations]) => {
-      this.translations.en = enTranslations;
-      this.translations.zh = zhTranslations;
-      this.renderTranslations();
-    });
+  async loadTranslations() {
+    const enTranslationsPromise = fetch("en.json").then(response => response.json());
+    const zhTranslationsPromise = fetch("zh.json").then(response => response.json());
+    const [enTranslations, zhTranslations] = await Promise.all([enTranslationsPromise, zhTranslationsPromise]);
+    this.translations.en = enTranslations;
+    this.translations.zh = zhTranslations;
   }
 
   saveTranslation(key, value) {
-    this.translations[this.currentLang][key] = value;
+    const keys = key.split('.');
+    let obj = this.translations[this.currentLang];
+    for (const k of keys.slice(0, -1)) {
+      obj = obj[k];
+    }
+    obj[keys.slice(-1)] = value;
     localStorage.setItem("translations", JSON.stringify(this.translations));
   }
 
   getCurrentTranslations() {
-    const translationsString = localStorage.getItem("translations");
-    const translations = JSON.parse(translationsString) || {};
-    return translations[this.currentLang] || {};
+    return this.translations[this.currentLang];
   }
 
   getTranslation(key) {
-    const translation = this.getCurrentTranslations()[key];
-    return typeof translation === "undefined" ? "" : translation;
+    const keys = key.split('.');
+    let obj = this.translations[this.currentLang];
+    for (const k of keys) {
+      obj = obj[k];
+    }
+    return obj;
   }
 
   setCurrentLang(lang) {
@@ -68,19 +75,110 @@ class I18nManager {
 }
 
 const i18nManager = new I18nManager();
+i18nManager.init();
 
-document.getElementById("en-link").addEventListener("click", () => {
-  i18nManager.setCurrentLang("en");
-});
+async init() {
+    await this.loadTranslations();
+    this.renderTranslations();
+    document.getElementById("en-link").addEventListener("click", () => {
+      this.setCurrentLang("en");
+    });
+    document.getElementById("zh-link").addEventListener("click", () => {
+      this.setCurrentLang("zh");
+    });
+    document.getElementById("export-en-button").addEventListener("click", () => {
+      this.exportTranslations("en");
+    });
+    document.getElementById("export-zh-button").addEventListener("click", () => {
+      this.exportTranslations("zh");
+    });
+  }
 
-document.getElementById("zh-link").addEventListener("click", () => {
-  i18nManager.setCurrentLang("zh");
-});
+  async loadTranslations() {
+    const enTranslationsPromise = fetch("en.json").then(response => response.json());
+    const zhTranslationsPromise = fetch("zh.json").then(response => response.json());
+    const [enTranslations, zhTranslations] = await Promise.all([enTranslationsPromise, zhTranslationsPromise]);
+    this.translations.en = enTranslations;
+    this.translations.zh = zhTranslations;
+  }
 
-document.getElementById("export-en-button").addEventListener("click", () => {
-  i18nManager.exportTranslations("en");
-});
+  saveTranslation(key, value) {
+    const keys = key.split('.');
+    let obj = this.translations[this.currentLang];
+    for (const k of keys.slice(0, -1)) {
+      obj = obj[k];
+    }
+    obj[keys.slice(-1)] = value;
+    localStorage.setItem("translations", JSON.stringify(this.translations));
+  }
 
-document.getElementById("export-zh-button").addEventListener("click", () => {
-  i18nManager.exportTranslations("zh");
-});
+  getCurrentTranslations() {
+    return this.translations[this.currentLang];
+  }
+
+  getTranslation(key) {
+    const keys = key.split('.');
+    let obj = this.translations[this.currentLang];
+    for (const k of keys) {
+      obj = obj[k];
+    }
+    return obj;
+  }
+
+  setCurrentLang(lang) {
+    this.currentLang = lang;
+    this.renderTranslations();
+  }
+
+  renderTranslations() {
+    const container = document.getElementById("translations-container");
+    container.innerHTML = "";
+    const translations = this.getCurrentTranslations();
+    for (const [key, value] of Object.entries(translations)) {
+      const translation = document.createElement("div");
+      translation.classList.add("translation");
+      const keyElement = document.createElement("p");
+      keyElement.classList.add("key");
+      keyElement.textContent = key;
+      translation.appendChild(keyElement);
+      const valueElement = document.createElement("textarea");
+      valueElement.classList.add("value");
+      valueElement.textContent = value;
+      valueElement.addEventListener("change", () => this.saveTranslation(key, valueElement.value));
+      translation.appendChild(valueElement);
+      container.appendChild(translation);
+      const subKeys = Object.keys(value);
+      if (subKeys.length > 0) {
+        const subTranslationsContainer = document.createElement("div");
+        subTranslationsContainer.classList.add("sub-translations");
+        translation.appendChild(subTranslationsContainer);
+        for (const subKey of subKeys) {
+          const subTranslation = document.createElement("div");
+          subTranslation.classList.add("translation");
+          const subKeyElement = document.createElement("p");
+          subKeyElement.classList.add("key");
+          subKeyElement.textContent = subKey;
+          subTranslation.appendChild(subKeyElement);
+          const subValueElement = document.createElement("textarea");
+          subValueElement.classList.add("value");
+          subValueElement.textContent = value[subKey];
+          subValueElement.addEventListener("change", () => this.saveTranslation(`${key}.${subKey}`, subValueElement.value));
+          subTranslation.appendChild(subValueElement);
+          subTranslationsContainer.appendChild(subTranslation);
+        }
+      }
+    }
+  }
+
+  exportTranslations(lang) {
+    const translations = this.translations[lang];
+    const blob = new Blob([JSON.stringify(translations, null, 2)], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = `i18n-${lang}.json`;
+    a.click();
+  }
+
+const i18nManager = new I18nManager();
+i18nManager.init();
